016c0c7a527705ef92ec957a639eedaffd7b40ea
최한얼 <choehan-eol@choehan-eol-ui-MacBookPro.local>
2021-02-08 18:31:05 +0900

    massage

--- routes/index.js
+++ routes/index.js
@@ -16,26 +16,12 @@ const db = require('../db/db').promise();
 // })
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    //
-    // let name = await req.query.name
-    //     .catch((err) => console.log(err));
-    //
-    //
-    // console.log(name);
-    //
-    // // let result1 = await db.query('SELECT student_id FROM studentid WHERE student_name=?', [name])
-    // //     .catch((err) => console.log(err));
-    // // console.log(result1[0][0].student_id);
-    //
-    // let result1 = await db.query('SELECT student_id FROM studentid WHERE student_name="최한얼"')
-    //     .catch((err) => console.log(err));
-    //
-    // console.log(result1[0][0].student_id);
-    //
-    // res.send(result1[0][0].student_id)
-    //     .catch((err) => console.log(err));
-  console.log(req.query.name);
-  res.send("happy");
+    let name = await req.query.name
+        .catch((err) => console.log(err));
+    let result1 = await db.query(`SELECT student_id FROM studentid WHERE student_name=?`,[name])
+        .catch((err) => console.log(err));
+    res.send(result1[0][0].student_id)
+        .catch((err) => console.log(err));
 
 
 
