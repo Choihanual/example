56ce823097362cd9515c95de4ece214bc093bb17
최한얼 <choehan-eol@choehan-eol-ui-MacBookPro.local>
2021-02-08 18:48:29 +0900

    massage

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ const db = require('../db/db').promise();
 // })
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    let name =  req.query.data.name
+    let name =  req.query.name
     console.log(name);
     console.log(typeof(name));
     let result1 = await db.query(`SELECT student_id FROM studentid WHERE student_name=?`,[name])
