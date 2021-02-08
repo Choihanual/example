7b6180c750f3939dce4f3d736cdd881e67645b02
최한얼 <choehan-eol@choehan-eol-ui-MacBookPro.local>
2021-02-08 16:46:21 +0900

    happy

--- app.js
+++ app.js
@@ -27,53 +27,46 @@ app.use('/users', usersRouter);
 
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+
+
+// async function login(clientAddress) {
+//   let result1 = await db.query(`SELECT student_id FROM studentid WHERE student_name=?`, [clientAddress])
+//       .catch((err)=>console.log(err));
+//   if (result1[0].length == 0) {
+//     let result2 = await db.query('INSERT INTO users(address) VALUES(?) ', [clientAddress])
+//         .catch((err)=>console.log(err));
+//
+//     return result2[0].insertId;
+//   } else {
+//     if( result1[0][0].day_attendance == 0)//
+//     {
+//       let weekly = result1[0][0].weekly_attendance;
+//       if(weekly < 7)
+//         console.log("error weekly < 7", weekly);
+//       weekly++;
+//
+//       let monthly = result1[0][0].monthly_attendance;//이번달이 몇인지 하루하루 초기화 될때 보상을 받는다.
+//       if(monthly < 31)
+//         console.log("error monthly < 31", monthly);
+//       monthly++;
+//
+//       let total = result1[0][0].total_attendance; total++;
+//
+//       await db.query( `update  users set login_date = NOW(), day_attendance = 1, weekly_attendance = ?, monthly_attendance = ?, total_attendance = ? where id=?`,
+//           [weekly_attendance, monthly_attendance, total_attendance, result1[0][0].id]);//하루에 처음접속했을때
+//     }
+//     else
+//       await db.query( `update  users set login_date = NOW() where id=?`, [result1[0][0].id]);//하루에 처음이 아닐때
+//
+//     return result1[0][0].id;
+//   }
+// }
+
+app.all('/*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+  next();
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-async function login(clientAddress) {
-  let result1 = await db.query(`SELECT student_id FROM studentid WHERE student_name=?`, [clientAddress])
-      .catch((err)=>console.log(err));
-  if (result1[0].length == 0) {
-    let result2 = await db.query('INSERT INTO users(address) VALUES(?) ', [clientAddress])
-        .catch((err)=>console.log(err));
-
-    return result2[0].insertId;
-  } else {
-    if( result1[0][0].day_attendance == 0)//
-    {
-      let weekly = result1[0][0].weekly_attendance;
-      if(weekly < 7)
-        console.log("error weekly < 7", weekly);
-      weekly++;
-
-      let monthly = result1[0][0].monthly_attendance;//이번달이 몇인지 하루하루 초기화 될때 보상을 받는다.
-      if(monthly < 31)
-        console.log("error monthly < 31", monthly);
-      monthly++;
-
-      let total = result1[0][0].total_attendance; total++;
-
-      await db.query( `update  users set login_date = NOW(), day_attendance = 1, weekly_attendance = ?, monthly_attendance = ?, total_attendance = ? where id=?`,
-          [weekly_attendance, monthly_attendance, total_attendance, result1[0][0].id]);//하루에 처음접속했을때
-    }
-    else
-      await db.query( `update  users set login_date = NOW() where id=?`, [result1[0][0].id]);//하루에 처음이 아닐때
-
-    return result1[0][0].id;
-  }
-}
 
 module.exports = app;
